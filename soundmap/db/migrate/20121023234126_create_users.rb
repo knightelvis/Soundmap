41f1413b3a72173d1bee72e8ac3019e91f985814
Chen Liu <chenliu@169-231-105-247.wireless.ucsb.edu>
2012-10-23 17:04:32 -0700

    Create database

--- soundmap/db/migrate/20121023234126_create_users.rb
+++ soundmap/db/migrate/20121023234126_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table (:users, :primary_key =>'id') do |t|
+    create_table(:users, :primary_key =>'id') do |t|
       t.integer :id
       t.string :username
       t.string :password
