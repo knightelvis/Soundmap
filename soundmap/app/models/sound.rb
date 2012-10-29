6cad27d9b5dc200b4bfdf6f5309d6f15f2ece970
Chen Liu <chenliu@169-231-105-208.wireless.ucsb.edu>
2012-10-28 18:43:30 -0700

    upload3

--- soundmap/app/models/sound.rb
+++ soundmap/app/models/sound.rb
@@ -1,16 +1,17 @@
 class Sound < ActiveRecord::Base
   attr_accessible :date, :description, :id, :location, :path, :title, :username
 
-def self.save(upload)
-    name =  upload['sound'].original_filename
-	Rails.logger.error 'in save!!'
-Rails.logger.error name
+def self.uploadfile(upload)
+ Rails.logger.error 'in save!!'
+ Rails.logger.error upload['mysound']	   
+name =  upload['mysound'].original_filename
+	
+ Rails.logger.error "###############"
     directory = "public/data"
     # create the file path
     path = File.join(directory, name)
     # write the file
-    File.open(path, "wb") { |f| f.write(upload['sound'].read) }
-  end
-
-
+    File.open(path, "wb") { |f| f.write(upload['mysound'].read) }
+ 
+ end
 end
