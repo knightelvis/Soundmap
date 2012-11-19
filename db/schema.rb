9af86397aaa4930c8aa26c9e389dfcd1460eabf5
Susen Zhao <prcstyler@gmail.com>
2012-11-19 03:05:58 -0800

    schema file for Like model modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121114042836) do
+ActiveRecord::Schema.define(:version => 20121119084946) do
+
+  create_table "likes", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "sound_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "sounds", :force => true do |t|
     t.string   "title"
