2dcf41795b0f15ed4f766ced0e7f8244e9f667b4
Susen Zhao <susen@susen-HP-ProBook-4411s.(none)>
2012-12-08 19:47:04 -0800

    remove redirection for test

--- app/controllers/map_view_controller.rb
+++ app/controllers/map_view_controller.rb
@@ -1,15 +1,12 @@
 class MapViewController < ApplicationController
 
+  before_filter :authenticate_user!
+
   def index
 
     @current_page = 'home'
-
-    if user_signed_in?
-      @sounds = Sound.where("user_id = ?", current_user.id).order("updated_at DESC")
-      @feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 10)
-    else
-      redirect_to '/explore'
-    end
+    @sounds = Sound.where("user_id = ?", current_user.id).order("updated_at DESC")
+    @feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 10)
 
   end
 
